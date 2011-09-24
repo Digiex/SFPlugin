9335bb89869d3607da78048c869732600b8a25c3
jessenic <jessenic@digiex.net>
2011-09-24 17:33:02 +0300

    Improving /world even further

--- src/main/java/net/digiex/simplefeatures/commands/CMDworld.java
+++ src/main/java/net/digiex/simplefeatures/commands/CMDworld.java
@@ -21,13 +21,22 @@ public class CMDworld implements CommandExecutor {
 			String label, String[] args) {
 		if (sender instanceof Player) {
 			if (args.length > 0) {
-				World world = plugin.getServer().getWorld(args[0]);
+				World world = null;
+				for (World w : plugin.getServer().getWorlds()) {
+					if (w.getName().equalsIgnoreCase(args[0])) {
+						world = w;
+					} else if (w.getName().toLowerCase()
+							.contains(args[0].toLowerCase())) {
+						world = w;
+					}
+
+				}
 				if (world != null) {
 					if (world.getName().contains("_nether")) {
 						if (!((Player) sender).isOp()) {
 							sender.sendMessage(ChatColor.RED
 									+ "Only ops can warp to the nether!");
-							return false;
+							return true;
 						} else {
 							sender.sendMessage("Wait! You need to use nether portals!!! Oh you're an OP... Sorry, my mistake.");
 						}
@@ -37,17 +46,32 @@ public class CMDworld implements CommandExecutor {
 							+ world.getName());
 					return true;
 
-				} else {
-					sender.sendMessage(ChatColor.RED + "World \"" + args[0]
-							+ "\" was not found. Check Spelling.");
-					sender.sendMessage(ChatColor.GREEN + "Available worlds:");
-					for (World w : plugin.getServer().getWorlds()) {
-						sender.sendMessage(ChatColor.YELLOW + w.getName());
-					}
 				}
 			}
+			ListWorlds(sender, args[0]);
+			return true;
 		}
 		return false;
 	}
 
+	private void ListWorlds(CommandSender sender, String tried) {
+		sender.sendMessage(ChatColor.RED + "World \"" + tried
+				+ "\" was not found. Check Spelling.");
+		sender.sendMessage(ChatColor.GREEN + "Available worlds:");
+		for (World w : plugin.getServer().getWorlds()) {
+			if (w.getName().contains("_nether")) {
+				boolean allownether = false;
+				if (sender instanceof Player) {
+					allownether = ((Player) sender).isOp();
+				} else {
+					allownether = true;
+				}
+				if (allownether) {
+					sender.sendMessage(ChatColor.GRAY + w.getName());
+				}
+			} else {
+				sender.sendMessage(ChatColor.YELLOW + w.getName());
+			}
+		}
+	}
 }
