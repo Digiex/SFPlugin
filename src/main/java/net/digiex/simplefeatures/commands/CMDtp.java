0d72300586c6bc13b379a9254c046bcfd299542f
jessenic <jessenic@digiex.net>
2011-09-24 15:47:03 +0300

    /tp and /tphere for ops

---
+++ src/main/java/net/digiex/simplefeatures/commands/CMDtp.java
@@ -0,0 +1,39 @@
+package net.digiex.simplefeatures.commands;
+
+import net.digiex.simplefeatures.SFPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CMDtp implements CommandExecutor {
+
+    SFPlugin plugin;
+
+    public CMDtp(SFPlugin parent) {
+        this.plugin = parent;
+    }
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if(sender instanceof Player){
+			Player p = (Player) sender;
+			Player from = p;
+			Player to = null;
+			if(p.isOp()){
+				if(args.length == 1){
+					to = SFPlugin.getPlayer(sender, args[0]);
+				}else if(args.length == 2){
+					from = SFPlugin.getPlayer(sender, args[0]);
+					to = SFPlugin.getPlayer(sender, args[1]);
+				}
+				if(from != null && to != null){
+					from.teleport(to);
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ src/main/java/net/digiex/simplefeatures/commands/CMDtphere.java
@@ -0,0 +1,36 @@
+package net.digiex.simplefeatures.commands;
+
+import net.digiex.simplefeatures.SFPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CMDtphere implements CommandExecutor {
+
+    SFPlugin plugin;
+
+    public CMDtphere(SFPlugin parent) {
+        this.plugin = parent;
+    }
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if(sender instanceof Player){
+			Player p = (Player) sender;
+			Player from = null;
+			Player to = p;
+			if(p.isOp()){
+				if(args.length == 1){
+					from = SFPlugin.getPlayer(sender, args[0]);
+				}
+				if(from != null && to != null){
+					from.teleport(to);
+				}
+			}
+		}
+		return false;
+	}
+}
