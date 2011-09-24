3c56d2c854e9ac631bacb5ae1f8d0d97e2de9789
jessenic <jessenic@digiex.net>
2011-09-24 17:20:55 +0300

    Making /world command better

--- src/main/java/net/digiex/simplefeatures/commands/CMDworld.java
+++ src/main/java/net/digiex/simplefeatures/commands/CMDworld.java
@@ -9,29 +9,41 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CMDworld implements CommandExecutor{
+public class CMDworld implements CommandExecutor {
 	SFPlugin plugin;
-	public CMDworld(SFPlugin parent){
+
+	public CMDworld(SFPlugin parent) {
 		this.plugin = parent;
 	}
+
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(sender instanceof Player){
-			if(args.length > 0){
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if (sender instanceof Player) {
+			if (args.length > 0) {
 				World world = plugin.getServer().getWorld(args[0]);
-				if(world != null){
-					if(world.getName().contains("_nether")){
-						if(!((Player) sender).isOp()){
-							sender.sendMessage(ChatColor.RED+"Only ops can warp to the nether!");
+				if (world != null) {
+					if (world.getName().contains("_nether")) {
+						if (!((Player) sender).isOp()) {
+							sender.sendMessage(ChatColor.RED
+									+ "Only ops can warp to the nether!");
 							return false;
-						}else{
+						} else {
 							sender.sendMessage("Wait! You need to use nether portals!!! Oh you're an OP... Sorry, my mistake.");
 						}
 					}
 					((Player) sender).teleport(world.getSpawnLocation());
-					sender.sendMessage(ChatColor.GRAY+"Teleporting to "+world.getName());
+					sender.sendMessage(ChatColor.GRAY + "Teleporting to "
+							+ world.getName());
 					return true;
-					
+
+				} else {
+					sender.sendMessage(ChatColor.RED + "World \"" + args[0]
+							+ "\" was not found. Check Spelling.");
+					sender.sendMessage(ChatColor.GREEN + "Available worlds:");
+					for (World w : plugin.getServer().getWorlds()) {
+						sender.sendMessage(ChatColor.YELLOW + w.getName());
+					}
 				}
 			}
 		}
