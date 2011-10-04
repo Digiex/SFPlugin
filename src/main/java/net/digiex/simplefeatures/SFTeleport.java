56d90db834cc23b3fb2ef4de9271e7c9cd8ac847
jessenic <jessenic@digiex.net>
2011-10-04 16:55:07 +0300

    Cleaning the teleport task

--- src/main/java/net/digiex/simplefeatures/SFTeleport.java
+++ src/main/java/net/digiex/simplefeatures/SFTeleport.java
@@ -6,288 +6,240 @@ import org.bukkit.Location;
 
 public class SFTeleport {
 
-    private SFPlugin parent;
-    private Player from;
-    private Player to;
-    private World world;
-    private int id = -1;
-    private Location home;
-    private String question;
-    private boolean counting;
-    private boolean timer = true;
-    private TeleportTypes type = TeleportTypes.unknown;
+	private SFPlugin parent;
+	private Player from;
+	private Player to;
+	private World world;
+	private int id = -1;
+	private Location home;
+	private String question;
+	private boolean counting;
+	private boolean timer = true;
+	private TeleportTypes type = TeleportTypes.unknown;
 
-    public SFTeleport(SFPlugin parent, TeleportTypes type) {
-        this.parent = parent;
-        this.type = type;
-    }
+	public SFTeleport(SFPlugin parent, TeleportTypes type) {
+		this.parent = parent;
+		this.type = type;
+	}
 
-    public TeleportTypes getType() {
-        return this.type;
-    }
+	public TeleportTypes getType() {
+		return this.type;
+	}
 
-    public Player getFrom() {
-        return this.from;
-    }
-    
-    public void setFrom(Player from) {
-        this.from = from;
-    }
+	public Player getFrom() {
+		return this.from;
+	}
 
-    public Player getTo() {
-        return this.to;
-    }
-    
-    public Location getHome() {
-        return this.home;
-    }
-    
-    public void setHome(Location home) {
-        this.home = home;
-    }
-    
-    public void setTo(Player to) {
-        this.to = to;
-    }
+	public void setFrom(Player from) {
+		this.from = from;
+	}
 
-    public World getWorld() {
-        return this.world;
-    }
-    
-    public void setWorld(World world) {
-        this.world = world;
-    }
+	public Player getTo() {
+		return this.to;
+	}
 
-    public int getId() {
-        return this.id;
-    }
+	public Location getHome() {
+		return this.home;
+	}
 
-    public String getQuestion() {
-        return this.question;
-    }
+	public void setHome(Location home) {
+		this.home = home;
+	}
 
-    public boolean getTimer() {
-        return this.timer;
-    }
-    
-    public void setTimer(boolean timer) {
-        this.timer = timer;
-    }
-    
-    public boolean getCounting() {
-        return this.counting;
-    }
+	public void setTo(Player to) {
+		this.to = to;
+	}
 
-    public int startTeleport() {
-        SFTeleportTask task = new SFTeleportTask();
-        return this.id = parent.getServer().getScheduler().scheduleAsyncDelayedTask(parent, task);
-    }
+	public World getWorld() {
+		return this.world;
+	}
 
-    private String setQuestion() {
-        if (getType().equals(TeleportTypes.tpa)) {
-            return getFrom().getDisplayName() + " wants to teleport to you. Do you want to accept?";
-        } else if (getType().equals(TeleportTypes.tpahere)) {
-            return getFrom().getDisplayName() + " wants to teleport you to him/her. Do you want to accept?";
-        }
-        return "Uhh oh, something has went wrong, please shout at the feature commander.";
-    }
+	public void setWorld(World world) {
+		this.world = world;
+	}
 
-    public enum TeleportTypes {
+	public int getId() {
+		return this.id;
+	}
 
-        tpa, tpahere, spawn, home, world, unknown
-    }
+	public String getQuestion() {
+		return this.question;
+	}
 
-    private class SFTeleportTask implements Runnable {
+	public boolean getTimer() {
+		return this.timer;
+	}
 
-        @Override
-        public void run() {
-            question = setQuestion();
-            if (getType().equals(TeleportTypes.tpa)) {
-                if (SFPlugin.questioner.ask(getTo(), question, "yes", "no").equals("yes")) {
-                    getFrom().sendMessage("Teleport request accepted");
-                    getTo().sendMessage("Teleport request accepted");
-                    startCountDown();
-                    parent.teleporters.remove(getFrom());
-                } else {
-                    getFrom().sendMessage("Teleport request rejected");
-                    getTo().sendMessage("Teleport request rejected");
-                    parent.teleporters.remove(getFrom());
-                }
-            } else if (getType().equals(TeleportTypes.tpahere)) {
-                if (SFPlugin.questioner.ask(getTo(), question, "yes", "no").equals("yes")) {
-                    getFrom().sendMessage("Teleport request accepted");
-                    getTo().sendMessage("Teleport request accepted");
-                    startCountDown();
-                    parent.teleporters.remove(getTo());
-                } else {
-                    getFrom().sendMessage("Teleport request rejected");
-                    getTo().sendMessage("Teleport request rejected");
-                    parent.teleporters.remove(getTo());
-                }
-            } else if (getType().equals(TeleportTypes.home)) {
-                startCountDown();
-                parent.teleporters.remove(getFrom());
-            } else if (getType().equals(TeleportTypes.spawn)) {
-                startCountDown();
-                parent.teleporters.remove(getFrom());
-            } else if (getType().equals(TeleportTypes.world)) {
-                startCountDown();
-                parent.teleporters.remove(getFrom());
-            }
-        }
+	public void setTimer(boolean timer) {
+		this.timer = timer;
+	}
 
-        private void startCountDown() {
-            if (getType().equals(TeleportTypes.tpa)) {
-                if (getTimer()) {
-                    counting = true;
-                    try {
-                        getFrom().sendMessage("You will be teleported to " + getTo().getDisplayName() + " in 30 seconds...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("20...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("10...");
-                        Thread.sleep(5000);
-                        getFrom().sendMessage("5...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("4...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("3...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("2...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("1...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("Poof!");
-                        getFrom().teleport(getTo());
-                    } catch (InterruptedException e) {
-                        parent.teleporters.remove(getFrom());
-                    }
-                } else {
-                    getFrom().teleport(getTo());
-                    getFrom().sendMessage("Poof!");
-                    parent.teleporters.remove(getFrom());
-                }
-            } else if (getType().equals(TeleportTypes.tpahere)) {
-                if (getTimer()) {
-                    counting = true;
-                    try {
-                        getTo().sendMessage("You will be teleported to " + getFrom().getDisplayName() + " in 30 seconds...");
-                        Thread.sleep(10000);
-                        getTo().sendMessage("20...");
-                        Thread.sleep(10000);
-                        getTo().sendMessage("10...");
-                        Thread.sleep(5000);
-                        getTo().sendMessage("5...");
-                        Thread.sleep(1000);
-                        getTo().sendMessage("4...");
-                        Thread.sleep(1000);
-                        getTo().sendMessage("3...");
-                        Thread.sleep(1000);
-                        getTo().sendMessage("2...");
-                        Thread.sleep(1000);
-                        getTo().sendMessage("1...");
-                        Thread.sleep(1000);
-                        getTo().sendMessage("Poof!");
-                        getTo().teleport(getFrom());
-                    } catch (InterruptedException e) {
-                        parent.teleporters.remove(getTo());
-                    }
-                } else {
-                    getTo().teleport(getFrom());
-                    getTo().sendMessage("Poof!");
-                    parent.teleporters.remove(getTo());
-                }
-            } else if (getType().equals(TeleportTypes.home)) {
-                if (getTimer()) {
-                    counting = true;
-                    try {
-                        getFrom().sendMessage("You will be teleported home in 30 seconds...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("20...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("10...");
-                        Thread.sleep(5000);
-                        getFrom().sendMessage("5...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("4...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("3...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("2...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("1...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("Poof!");
-                        getFrom().teleport(getHome());
-                    } catch (InterruptedException e) {
-                        parent.teleporters.remove(getFrom());
-                    }
-                } else {
-                    getFrom().teleport(getHome());
-                    getFrom().sendMessage("Poof!");
-                    parent.teleporters.remove(getFrom());
-                }
-            } else if (getType().equals(TeleportTypes.spawn)) {
-                if (getTimer()) {
-                    counting = true;
-                    try {
-                        getFrom().sendMessage("You will be teleported to spawn in 30 seconds...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("20...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("10...");
-                        Thread.sleep(5000);
-                        getFrom().sendMessage("5...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("4...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("3...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("2...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("1...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("Poof!");
-                        getFrom().teleport(getFrom().getWorld().getSpawnLocation());
-                    } catch (InterruptedException e) {
-                        parent.teleporters.remove(getFrom());
-                    }
-                } else {
-                    getFrom().teleport(getFrom().getWorld().getSpawnLocation());
-                    getFrom().sendMessage("Poof!");
-                    parent.teleporters.remove(getFrom());
-                }
-            } else if (getType().equals(TeleportTypes.world)) {
-                if (getTimer()) {
-                    counting = true;
-                    try {
-                        getFrom().sendMessage("You will be teleported to " + getWorld().getName() + " in 30 seconds...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("20...");
-                        Thread.sleep(10000);
-                        getFrom().sendMessage("10...");
-                        Thread.sleep(5000);
-                        getFrom().sendMessage("5...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("4...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("3...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("2...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("1...");
-                        Thread.sleep(1000);
-                        getFrom().sendMessage("Poof!");
-                        getFrom().teleport(getWorld().getSpawnLocation());
-                    } catch (InterruptedException e) {
-                        parent.teleporters.remove(getFrom());
-                    }
-                } else {
-                    getFrom().teleport(getWorld().getSpawnLocation());
-                    getFrom().sendMessage("Poof!");
-                    parent.teleporters.remove(getFrom());
-                }
-            }
-        }
-    }
+	public boolean getCounting() {
+		return this.counting;
+	}
+
+	public int startTeleport() {
+		SFTeleportTask task = new SFTeleportTask();
+		return this.id = parent.getServer().getScheduler()
+				.scheduleAsyncDelayedTask(parent, task);
+	}
+
+	private void setQuestion() {
+		switch (getType()) {
+		case tpa:
+			question = getFrom().getDisplayName()
+					+ " wants to teleport to you. Do you want to accept?";
+			break;
+		case tpahere:
+			question = getFrom().getDisplayName()
+					+ " wants to teleport you to him/her. Do you want to accept?";
+			break;
+		default:
+			question = "Uhh oh, something has went wrong, please shout at the feature commander.";
+		}
+	}
+
+	public enum TeleportTypes {
+
+		tpa, tpahere, spawn, home, world, unknown
+	}
+
+	public enum TPAnswers {
+		yes, no
+	}
+
+	public TPAnswers getAnswer(String ans) {
+		if (ans == "yes")
+			return TPAnswers.yes;
+
+		return TPAnswers.no;
+	}
+
+	public boolean shouldIaskQuestions() {
+		switch (getType()) {
+		case tpa:
+		case tpahere:
+			return true;
+		default:
+			return false;
+		}
+	}
+
+	public void removeFromTPList() {
+		switch (getType()) {
+		case tpa:
+		case home:
+		case spawn:
+		case world:
+			parent.teleporters.remove(getFrom());
+			break;
+		case tpahere:
+			parent.teleporters.remove(getTo());
+			break;
+		}
+	}
+
+	private class SFTeleportTask implements Runnable {
+
+		@Override
+		public void run() {
+			setQuestion();
+			TPAnswers answer = getAnswer(SFPlugin.questioner.ask(getTo(),
+					question, "yes", "no"));
+			switch (answer) {
+			case yes:
+				if (shouldIaskQuestions()) {
+					getFrom().sendMessage("Teleport request accepted");
+					getTo().sendMessage("Teleport request accepted");
+				}
+				startCountDown();
+				removeFromTPList();
+				break;
+			case no:
+				if (shouldIaskQuestions()) {
+					getFrom().sendMessage("Teleport request rejected");
+					getTo().sendMessage("Teleport request rejected");
+				}
+				removeFromTPList();
+				break;
+			}
+		}
+
+		private void doTeleport() {
+			switch (getType()) {
+			case tpa:
+				getFrom().teleport(getTo());
+				break;
+			case tpahere:
+				getTo().teleport(getFrom());
+			case home:
+				getFrom().teleport(getHome());
+				break;
+			case spawn:
+				getFrom().teleport(getFrom().getWorld().getSpawnLocation());
+				break;
+			case world:
+				getFrom().teleport(getWorld().getSpawnLocation());
+				break;
+			}
+		}
+
+		private void startCountDown() {
+			Player fromP = getFrom();
+			String infoMsg = "";
+			switch (getType()) {
+			case tpa:
+				infoMsg = "You will be teleported to " + to.getDisplayName()
+						+ " in 30 seconds...";
+				break;
+			case tpahere:
+				fromP = getTo();
+				infoMsg = "You will be teleported to " + to.getDisplayName()
+						+ " in 30 seconds...";
+				break;
+			case home:
+				infoMsg = "You will be teleported home in 30 seconds...";
+				break;
+			case spawn:
+				infoMsg = "You will be teleported to spawn in 30 seconds...";
+				break;
+			case world:
+				infoMsg = "You will be teleported to " + getWorld().getName()
+						+ " in 30 seconds...";
+				break;
+			}
+
+			if (getTimer()) {
+				counting = true;
+				try {
+					fromP.sendMessage(infoMsg);
+
+					Thread.sleep(10000);
+					fromP.sendMessage("20...");
+					Thread.sleep(10000);
+					fromP.sendMessage("10...");
+					Thread.sleep(5000);
+					fromP.sendMessage("5...");
+					Thread.sleep(1000);
+					fromP.sendMessage("4...");
+					Thread.sleep(1000);
+					fromP.sendMessage("3...");
+					Thread.sleep(1000);
+					fromP.sendMessage("2...");
+					Thread.sleep(1000);
+					fromP.sendMessage("1...");
+					Thread.sleep(1000);
+					fromP.sendMessage("Poof!");
+					doTeleport();
+				} catch (InterruptedException e) {
+					removeFromTPList();
+				}
+			} else {
+				doTeleport();
+				fromP.sendMessage("Poof!");
+				removeFromTPList();
+			}
+
+		}
+	}
 }
