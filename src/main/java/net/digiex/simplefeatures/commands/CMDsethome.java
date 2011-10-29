Digiex/SFPlugin

commit 3d6cfaaa213d23fad65ce5472100b3c4cffe3e27

Fixed homes, issue #7

src/main/java/net/digiex/simplefeatures/commands/CMDsethome.java
@@ -34,13 +34,14 @@ public class CMDsethome implements CommandExecutor {
 			return true;
 		}
 		SFHome home = plugin.getDatabase().find(SFHome.class).where()
-				.ieq("worldName", player.getLocation().getWorld().getName()).ieq("playerName", player.getName())
-				.findUnique();
-		if (home == null) {
-			home = new SFHome();
-			home.setPlayer(player);
+				.ieq("worldName", player.getLocation().getWorld().getName())
+				.ieq("playerName", player.getName()).findUnique();
+		if (home != null) {
+			plugin.getDatabase().delete(home);
 		}
-		home.setLocation(((Player) sender).getLocation());
+		home = new SFHome();
+		home.setPlayer(player);
+		home.setLocation(player.getLocation());
 		plugin.getDatabase().save(home);
 		return true;
 	}

src/main/java/net/digiex/simplefeatures/listeners/PListener.java
@@ -73,10 +73,11 @@ public class PListener extends PlayerListener {
 						.ieq("worldName",
 								player.getLocation().getWorld().getName())
 						.ieq("playerName", player.getName()).findUnique();
-				if (home == null) {
-					home = new SFHome();
-					home.setPlayer(player);
+				if (home != null) {
+					plugin.getDatabase().delete(home);
 				}
+				home = new SFHome();
+				home.setPlayer(player);
 				home.setLocation(player.getLocation());
 				plugin.getDatabase().save(home);
 				player.sendMessage(ChatColor.YELLOW
@@ -114,10 +115,12 @@ public class PListener extends PlayerListener {
 			e.getPlayer().setPlayerListName(plistname);
 		}
 		List<SFMail> msgs;
-			msgs = plugin.getDatabase().find(SFMail.class).where()
-					.ieq("toPlayer", e.getPlayer().getName()).findList();
+		msgs = plugin.getDatabase().find(SFMail.class).where()
+				.ieq("toPlayer", e.getPlayer().getName()).findList();
 		if (!msgs.isEmpty()) {
-		e.getPlayer().sendMessage(ChatColor.AQUA+"You have "+msgs.size()+" new mail! Type /read to view.");
+			e.getPlayer().sendMessage(
+					ChatColor.AQUA + "You have " + msgs.size()
+							+ " new mail! Type /read to view.");
 		}
 	}
 

commit 5a04f08a9e03a82aecf09285e4630ac41f776f15

Fixing a home bug caused by commit 3d6cfaaa213d23fad65ce5472100b3c4cffe3e27

src/main/java/net/digiex/simplefeatures/commands/CMDsethome.java
@@ -39,10 +39,11 @@ public class CMDsethome implements CommandExecutor {
 		if (home != null) {
 			plugin.getDatabase().delete(home);
 		}
-		home = new SFHome();
-		home.setPlayer(player);
-		home.setLocation(player.getLocation());
-		plugin.getDatabase().save(home);
+		SFHome newhome = new SFHome();
+		newhome.setPlayer(player);
+		newhome.setLocation(player.getLocation());
+		plugin.getDatabase().save(newhome);
+		player.sendMessage(ChatColor.YELLOW+"Home for this world set!");
 		return true;
 	}
 

src/main/java/net/digiex/simplefeatures/listeners/PListener.java
@@ -10,6 +10,7 @@ import net.digiex.simplefeatures.SFPlugin;
 
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -60,7 +61,7 @@ public class PListener extends PlayerListener {
 
 		@Override
 		public void run() {
-
+			Location homeLoc = player.getLocation();
 			String answer = SFPlugin.questioner.ask(this.player,
 					ChatColor.YELLOW
 							+ "Do you want to set your home to this bed?",
@@ -76,10 +77,10 @@ public class PListener extends PlayerListener {
 				if (home != null) {
 					plugin.getDatabase().delete(home);
 				}
-				home = new SFHome();
-				home.setPlayer(player);
-				home.setLocation(player.getLocation());
-				plugin.getDatabase().save(home);
+				SFHome newhome = new SFHome();
+				newhome.setPlayer(player);
+				newhome.setLocation(homeLoc);
+				plugin.getDatabase().save(newhome);
 				player.sendMessage(ChatColor.YELLOW
 						+ "Your home for this world is now set to this bed!");
 			} else {