737a7a3b0530df03a144eb29a0bde663ad9e8954
jessenic <jessenic@digiex.net>
2011-12-17 15:30:53 +0200

    Players can be teleported from console now

--- src/main/java/net/digiex/simplefeatures/commands/CMDtp.java
+++ src/main/java/net/digiex/simplefeatures/commands/CMDtp.java
@@ -9,35 +9,45 @@ import org.bukkit.entity.Player;
 
 public class CMDtp implements CommandExecutor {
 
-    SFPlugin plugin;
+	SFPlugin plugin;
 
-    public CMDtp(SFPlugin parent) {
-        this.plugin = parent;
-    }
+	public CMDtp(SFPlugin parent) {
+		plugin = parent;
+	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
-		if(sender instanceof Player){
+		if (sender instanceof Player) {
 			Player p = (Player) sender;
 			Player from = p;
 			Player to = null;
-			if(p.isOp()){
-				if(args.length == 1){
+			if (p.isOp()) {
+				if (args.length == 1) {
 					to = SFPlugin.getPlayer(sender, args[0]);
-				}else if(args.length == 2){
+				} else if (args.length == 2) {
 					from = SFPlugin.getPlayer(sender, args[0]);
 					to = SFPlugin.getPlayer(sender, args[1]);
 				}
-				if(from != null && to != null){
+				if (from != null && to != null) {
 					from.teleport(to);
 					sender.sendMessage("Teleported ;)");
 					return true;
 				}
 			} else {
-                            p.sendMessage("Please use /TPA to Teleport to players");
-                            return true;
-                        }
+				p.sendMessage("Please use /TPA to Teleport to players");
+				return true;
+			}
+		} else {
+			if (args.length == 2) {
+				Player from = SFPlugin.getPlayer(sender, args[0]);
+				Player to = SFPlugin.getPlayer(sender, args[1]);
+				if (from != null && to != null) {
+					from.teleport(to);
+					sender.sendMessage("Teleported ;)");
+					return true;
+				}
+			}
 		}
 		return false;
 	}
