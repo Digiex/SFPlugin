644f70510da337d5a2daa5643dd4a9b441cdfb3f
jessenic <jessenic@digiex.net>
2012-03-01 22:02:48 +0200

    Made the /entities command work better

--- src/main/java/net/digiex/simplefeatures/commands/CMDentities.java
+++ src/main/java/net/digiex/simplefeatures/commands/CMDentities.java
@@ -1,19 +1,16 @@
 package net.digiex.simplefeatures.commands;
 
+import java.util.HashMap;
+
 import net.digiex.simplefeatures.SFPlugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.ExperienceOrb;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Minecart;
-import org.bukkit.entity.Painting;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 
 public class CMDentities implements CommandExecutor {
 	SFPlugin plugin;
@@ -43,57 +40,26 @@ public class CMDentities implements CommandExecutor {
 					}
 				}
 				sender.sendMessage("Entities for " + w.getName());
-				int expOrbs = 0;
-				int creatures = 0;
-				int items = 0;
-				int projectiles = 0;
-				int minecarts = 0;
-				int paintings = 0;
-				int players = 0;
+				HashMap<String, Integer> entities = new HashMap<String, Integer>();
 				for (Entity e : w.getEntities()) {
-					if (e instanceof ExperienceOrb) {
-						expOrbs++;
+					String name = "UNKNOWN";
+					if (e.getType() != null) {
+						name = e.getType().toString();
+					} else if (e.getClass().getCanonicalName() != null) {
+						name = e.getClass().getCanonicalName();
 					}
-					if (e instanceof Creature) {
-						creatures++;
+					int count = 0;
+					if (entities.get(name) != null) {
+						count = entities.get(name);
 					}
-					if (e instanceof Item) {
-						items++;
-					}
-					if (e instanceof Projectile) {
-						projectiles++;
-					}
-					if (e instanceof Minecart) {
-						minecarts++;
-					}
-					if (e instanceof Painting) {
-						paintings++;
-					}
-					if (e instanceof Player) {
-						players++;
-					}
+					entities.put(name, count + 1);
 				}
-				sender.sendMessage("All entities: " + w.getEntities().size());
-				if (expOrbs > 0) {
-					sender.sendMessage("Experience orbs: " + expOrbs);
-				}
-				if (creatures > 0) {
-					sender.sendMessage("Creatures: " + creatures);
-				}
-				if (items > 0) {
-					sender.sendMessage("Items: " + items);
-				}
-				if (projectiles > 0) {
-					sender.sendMessage("Projectiles: " + projectiles);
-				}
-				if (minecarts > 0) {
-					sender.sendMessage("Minecarts: " + minecarts);
-				}
-				if (paintings > 0) {
-					sender.sendMessage("Paintings: " + paintings);
-				}
-				if (players > 0) {
-					sender.sendMessage("Players: " + players);
+				sender.sendMessage(ChatColor.YELLOW + "All entities: "
+						+ ChatColor.AQUA + w.getEntities().size());
+				for (String name : entities.keySet()) {
+					sender.sendMessage(ChatColor.YELLOW
+							+ name.toLowerCase().replace("_", " ") + ": "
+							+ ChatColor.AQUA + entities.get(name));
 				}
 				return true;
 			}
